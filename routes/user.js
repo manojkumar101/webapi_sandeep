0333df0fd70039eea2c46deba533a599743a97f1
MANOJ KUMAR <manojkumar49490@gmail.com>
2021-10-20 11:52:13 +0530

    testest

--- routes/user.js
+++ routes/user.js
@@ -33,9 +33,7 @@ function firstAPI(req,res) {
 async function secondAPI(req,res) {
   try {
     let userINfo=res.locals.tokenInfo
-    console.log(" ~ userINfo", userINfo)
-    console.log(" ~ userINfo", userINfo)
-    console.log(" ~ userINfo", userINfo)
+  
     console.log("ndAPI ~ req", req.body.name)
   const cacheResponseQuery=`select * from service_request_cache where id = ?`;
   const result= await  query(cacheResponseQuery,[userINfo.id])
